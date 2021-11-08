a5a05e2c742110f49b3f17b1d0c5e86054aed332
Richard Lindsley <lindsley@remss.com>
2021-11-08 14:40:20 -0800

    Bump to version 0.2.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ extensions = [
 
 setup(
     name="access-atmosphere",
-    version="0.1.0",
+    version="0.2.0",
     packages=["access_atmosphere"],
     python_requires=">=3.7",
     install_requires=[
