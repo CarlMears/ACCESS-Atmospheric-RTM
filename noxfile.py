c75decd1e84c8aca432ebb6b82b163d5833e7c30
Richard Lindsley <lindsley@remss.com>
2022-07-29 16:58:17 -0700

    Add stubtest and pdoc to nox

--- noxfile.py
+++ noxfile.py
@@ -8,9 +8,7 @@ nox.options.sessions = ["black", "flake8", "mypy"]
 def black(session: nox.Session) -> None:
     """Check if black needs to be run"""
     session.install("black")
-    session.run(
-        "black", "--check", "--target-version=py38", "--diff", "python/"
-    )
+    session.run("black", "--check", "--target-version=py38", "--diff", "python/")
 
 
 @nox.session
@@ -45,3 +43,60 @@ def mypy_full(session: nox.Session) -> None:
         "--lineprecision-report=.",
         "python/",
     )
+
+
+@nox.session
+def stubtest(session: nox.Session) -> None:
+    """Run stubtest
+
+    The package needs to be built and installed before stubtest can import it.
+    If no arguments are specified, then the package is built here. This requires
+    Rust.
+
+    ```
+    nox -s stubtest
+    ```
+
+    Otherwise, a pre-built wheel can used, which avoids the need for Rust.
+
+    ```
+    nox -s stubtest -- *-abi3-*.whl
+    ```
+    """
+    session.install("mypy")
+    if session.posargs:
+        session.install(*session.posargs)
+    else:
+        session.log("Rebuilding the access_atmosphere package locally")
+        session.install(".")
+    session.run(
+        "stubtest",
+        "--mypy-config-file=pyproject.toml",
+        "access_atmosphere.access_atmosphere",
+    )
+
+
+@nox.session
+def pdoc(session: nox.Session) -> None:
+    """Run pdoc and save output
+
+    The package needs to be built and installed before pdoc can import it. If no
+    arguments are specified, then the package is built here. This requires Rust.
+
+    ```
+    nox -s pdoc
+    ```
+
+    Otherwise, a pre-built wheel can used, which avoids the need for Rust.
+
+    ```
+    nox -s pdoc -- *-abi3-*.whl
+    ```
+    """
+    if session.posargs:
+        session.install(*session.posargs)
+    else:
+        session.log("Rebuilding the access_atmosphere package locally")
+        session.install(".")
+    session.install("pdoc")
+    session.run("pdoc", "access_atmosphere", "--output-directory", "public")
