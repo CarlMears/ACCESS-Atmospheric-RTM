29ee5566ed66b965b027594bfd332c8c32c9c10f
Richard Lindsley <lindsley@remss.com>
2022-08-01 20:06:21 -0700

    Implement water vapor absorption coefficients

--- src/rtm/core.rs
+++ src/rtm/core.rs
@@ -5,20 +5,35 @@
 use once_cell::sync::OnceCell;
 use smallvec::SmallVec;
 
-const NLINES: usize = 44;
+const NLINES_O2: usize = 44;
 
 struct OxygenCoefficients {
-    f0: [f32; NLINES],
-    a1: [f32; NLINES],
-    a2: [f32; NLINES],
-    a3: [f32; NLINES],
-    a4: [f32; NLINES],
-    a5: [f32; NLINES],
-    a6: [f32; NLINES],
+    f0: [f32; NLINES_O2],
+    a1: [f32; NLINES_O2],
+    a2: [f32; NLINES_O2],
+    a3: [f32; NLINES_O2],
+    a4: [f32; NLINES_O2],
+    a5: [f32; NLINES_O2],
+    a6: [f32; NLINES_O2],
 }
 
 /// Oxygen absorption coefficients
-static OXY_COEF: OnceCell<OxygenCoefficients> = OnceCell::new();
+static O2_COEF: OnceCell<OxygenCoefficients> = OnceCell::new();
+
+const NLINES_H2O: usize = 15;
+
+struct WaterVaporCoefficients {
+    f0: [f32; NLINES_H2O],
+    b1: [f32; NLINES_H2O],
+    b2: [f32; NLINES_H2O],
+    b3: [f32; NLINES_H2O],
+    b4: [f32; NLINES_H2O],
+    b5: [f32; NLINES_H2O],
+    b6: [f32; NLINES_H2O],
+}
+
+/// Water vapor absorption coefficients
+static H2O_COEF: OnceCell<WaterVaporCoefficients> = OnceCell::new();
 
 /// Modified version of Liebe 1992 oxygen model.
 ///
@@ -39,9 +54,9 @@ pub(crate) fn fdabsoxy_1992_modified(p: f32, t: f32, pv: f32, freq: f32) -> f32 
         a4,
         a5,
         a6,
-    } = OXY_COEF.get_or_init(|| {
-        let mut a4 = [0.; NLINES];
-        for i in NLINES - 6..NLINES {
+    } = O2_COEF.get_or_init(|| {
+        let mut a4 = [0.; NLINES_O2];
+        for i in NLINES_O2 - 6..NLINES_O2 {
             a4[i] = 0.6;
         }
 
@@ -88,7 +103,7 @@ pub(crate) fn fdabsoxy_1992_modified(p: f32, t: f32, pv: f32, freq: f32) -> f32 
             -0.655, -0.660, -0.665, 0.008, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
         ];
 
-        let mut a1 = [0.; NLINES];
+        let mut a1 = [0.; NLINES_O2];
         a1.iter_mut().zip(&h2).zip(&h1).for_each(|((a1, h2), h1)| {
             *a1 = h2 / h1;
         });
@@ -114,7 +129,7 @@ pub(crate) fn fdabsoxy_1992_modified(p: f32, t: f32, pv: f32, freq: f32) -> f32 
     let pdry = 0.1 * p - pwet;
     let xterm = 1.0 - tht;
 
-    let sum: f64 = (0..NLINES)
+    let sum: f64 = (0..NLINES_O2)
         .into_iter()
         .map(|i| {
             let ga = a3[i] * (pdry * tht.powf(0.8 - a4[i]) + 1.1 * tht * pwet);
@@ -158,102 +173,147 @@ pub(crate) fn fdabsoxy_1992_modified(p: f32, t: f32, pv: f32, freq: f32) -> f32 
 /// From: P.W. Rosenkranz, Radio Science v.33, pp.919-928 (1998). Modified by
 /// Frank Wentz over the years and converted from Fortran to Rust by Richard
 /// Lindsley.
-pub(crate) fn abh2o_rk_modified(_p: f32, _t: f32, _pv: f32, _freq: f32) -> f32 {
-    //   integer(int32), parameter :: nlines=15
+pub(crate) fn abh2o_rk_modified(p: f32, t: f32, pv: f32, freq: f32) -> f32 {
+    // Many of the variables are retained from the original Fortran
+    let WaterVaporCoefficients {
+        f0,
+        b1,
+        b2,
+        b3,
+        b4,
+        b5,
+        b6,
+    } = H2O_COEF.get_or_init(|| {
+        // line frequencies
+        let f0 = [
+            22.2351, 183.3101, 321.2256, 325.1529, 380.1974, 439.1508, 443.0183, 448.0011,
+            470.8890, 474.6891, 488.4911, 556.9360, 620.7008, 752.0332, 916.1712,
+        ];
 
-    //   integer(int32) :: i
-    //   logical, save :: first = .true.
-    //   real(real32) :: s,base
-    //   real(real32) :: tht,pwet,pdry,ga,gasq,sftot,xterm,rnuneg,rnupos
-    //   real(real64) :: sum
+        // line intensities at 300 K
+        let b1: [f32; NLINES_H2O] = [
+            0.1310e-13, 0.2273e-11, 0.8036e-13, 0.2694e-11, 0.2438e-10, 0.2179e-11, 0.4624e-12,
+            0.2562e-10, 0.8369e-12, 0.3263e-11, 0.6659e-12, 0.1531e-08, 0.1707e-10, 0.1011e-08,
+            0.4227e-10,
+        ];
 
-    //   real(real32) :: chi,chisq,freqsq,f0sq,u,ffac
+        // t coeff. of intensities
+        let b2 = [
+            2.144, 0.668, 6.179, 1.541, 1.048, 3.595, 5.048, 1.405, 3.597, 2.379, 2.852, 0.159,
+            2.391, 0.396, 1.441,
+        ];
+        // air-broadened width parameters at 300 K
+        let mut b3 = [
+            0.0281, 0.0281, 0.023, 0.0278, 0.0287, 0.021, 0.0186, 0.0263, 0.0215, 0.0236, 0.026,
+            0.0321, 0.0244, 0.0306, 0.0267,
+        ];
+        // self-broadened width parameters at 300 K
+        let b5 = [
+            0.1349, 0.1491, 0.108, 0.135, 0.1541, 0.090, 0.0788, 0.1275, 0.0983, 0.1095, 0.1313,
+            0.1320, 0.1140, 0.1253, 0.1275,
+        ];
+        // t-exponent of air-broadening
+        let b4 = [
+            0.69, 0.64, 0.67, 0.68, 0.54, 0.63, 0.60, 0.66, 0.66, 0.65, 0.69, 0.69, 0.71, 0.68,
+            0.70,
+        ];
+        // t-exponent of self-broadening
+        let b6 = [
+            0.61, 0.85, 0.54, 0.74, 0.89, 0.52, 0.50, 0.67, 0.65, 0.64, 0.72, 1.0, 0.68, 0.84, 0.78,
+        ];
 
-    //   !     line frequencies:
-    //   real(real32), dimension(nlines), parameter :: f0 = [ &
-    //        22.2351, 183.3101, 321.2256, 325.1529, 380.1974, 439.1508, &
-    //        443.0183, 448.0011, 470.8890, 474.6891, 488.4911, 556.9360, &
-    //        620.7008, 752.0332, 916.1712]
-    //   !     line intensities at 300k:
-    //   real(real32), dimension(nlines), save :: b1 = [ &
-    //        .1310e-13, .2273e-11, .8036e-13, .2694e-11, .2438e-10, &
-    //        .2179e-11, .4624e-12, .2562e-10, .8369e-12, .3263e-11, .6659e-12, &
-    //        .1531e-08, .1707e-10, .1011e-08, .4227e-10]
-    //   !     t coeff. of intensities:
-    //   real(real32), dimension(nlines), parameter :: b2 = [ &
-    //        2.144, .668, 6.179, 1.541, 1.048, 3.595, 5.048, 1.405, 3.597, 2.379, 2.852, .159, 2.391, .396, 1.441]
-    //   !     air-broadened width parameters at 300k:
-    //   real(real32), dimension(nlines), save :: b3 = [ &
-    //        .0281, .0281, .023, .0278, .0287, .021, .0186, .0263, .0215, .0236, .026, .0321, .0244, .0306, .0267]
-    //   !     self-broadened width parameters at 300k:
-    //   real(real32), dimension(nlines), save :: b5 = [ &
-    //        .1349, .1491, .108, .135, .1541, .090, .0788, .1275, .0983, .1095, .1313, .1320, .1140, .1253, .1275]
-    //   !     t-exponent of air-broadening:
-    //   real(real32), dimension(nlines), parameter :: b4 = [ &
-    //        .69, .64, .67, .68, .54, .63, .60, .66, .66, .65, .69, .69, .71, .68, .70]
-    //   !     t-exponent of self-broadening:
-    //   real(real32), dimension(nlines), parameter :: b6 = [ &
-    //        .61, .85, .54, .74, .89, .52, .50, .67, .65, .64, .72, 1.0, .68, .84, .78]
+        let mut b1_modified = [0.; NLINES_H2O];
+        for ((b1_new, b1), &f0) in b1_modified.iter_mut().zip(&b1).zip(&f0) {
+            *b1_new = 1.8281089E+14 * b1 / f32::powi(f0, 2);
+        }
 
-    //   !$omp critical
-    //   if (first) then
-    //      first = .false.
-    //      b1=1.8281089E+14*b1/f0**2
-    //      b5=b5/b3  !convert b5 to Leibe notation
-    //      !1    b1(1)=1.01*b1(1)  !this modification is no longer done
-    //      b3(1)=b3(1)/1.040 !modification 1
-    //   end if
-    //   !$omp end critical
+        // convert b5 to Leibe notation
+        let mut b5_modified = [0.; NLINES_H2O];
+        for ((b5_new, b5), b3) in b5_modified.iter_mut().zip(&b5).zip(&b3) {
+            *b5_new = b5 / b3;
+        }
 
-    //   if (pv <= 0.) then
-    //      gamh2o=0
-    //      return
-    //   end if
+        // Modification 1
+        b3[0] /= 1.040;
 
-    //   pwet=0.1*pv
-    //   pdry=0.1*p-pwet
-    //   tht = 300./t
-    //   xterm=1-tht
-    //   freqsq=freq*freq
+        WaterVaporCoefficients {
+            f0,
+            b1: b1_modified,
+            b2,
+            b3,
+            b4,
+            b5: b5_modified,
+            b6,
+        }
+    });
 
-    //   sum = 0.
-    //   do i = 1, nlines
-    //      f0sq=f0(i)*f0(i)
-    //      ga=b3(i)*(pdry*tht**b4(i) + b5(i)*pwet*tht**b6(i))
-    //      gasq = ga*ga
-    //      s = b1(i)*exp(b2(i)*xterm)
-    //      rnuneg = f0(i)-freq
-    //      rnupos = f0(i)+freq
-    //      base = ga/(562500. + gasq)  !use clough's definition of local line contribution
+    if pv <= 0. {
+        return 0.;
+    }
 
-    //      if (i /= 1) then
-    //         if (abs(rnuneg) < 750) sum = sum + s*(ga/(gasq + rnuneg**2) - base)
-    //         if (abs(rnupos) < 750) sum = sum + s*(ga/(gasq + rnupos**2) - base)
-    //      else
-    //         chi=0.07*ga   !modification 2
+    let pwet = 0.1 * pv;
+    let pdry = 0.1 * p - pwet;
+    let tht = 300. / t;
+    let xterm = 1. - tht;
+    let freq_sq = freq.powi(2);
 
-    //         if (freq < 19) then
-    //            u=abs(freq-19.)/16.5
-    //            if (u < 0) u=0
-    //            if (u > 1) u=1
-    //            chi=0.07*ga + 0.93*ga*u*u*(3-2*u)  !modification 2
-    //         end if
+    let sum: f64 = (0..NLINES_H2O)
+        .into_iter()
+        .map(|i| {
+            let f0sq = f0[i].powi(2);
+            let ga = b3[i] * (pdry * tht.powf(b4[i]) + b5[i] * pwet * tht.powf(b6[i]));
+            let ga_sq = ga.powi(2);
+            let s = b1[i] * f32::exp(b2[i] * xterm);
+            let rnuneg = f0[i] - freq;
+            let rnupos = f0[i] + freq;
 
-    //         chisq=chi*chi
-    //         sum=sum + s*2*((ga-chi)*freqsq + (ga+chi)*(f0sq+gasq-chisq))/((freqsq-f0sq-gasq+chisq)**2 + 4*freqsq*gasq)
-    //      end if
-    //   end do
-    //   if (sum < 0) sum=0
+            // use clough's definition of local line contribution
+            let base = ga / (562_500. + ga_sq);
 
-    //   ffac=1
-    //   if (freq < 90) ffac=ffac + 0.1*((90. - freq)/90.)**1.4
-    //   !1    sftot=pwet*freq*tht**3.5*(sum +      1.1*1.2957246e-6*pdry/tht**0.5 + 0.425*(freq**0.10)*4.2952193e-5*pwet*tht**4) !previous version
-    //   sftot=real(pwet*freq*tht**3.5*(sum + ffac*1.1*1.2957246e-6*pdry/tht**0.5 + 0.348*(freq**0.15)*4.2952193e-5*pwet*tht**4), real32) !modification 3
+            if i != 0 {
+                let mut sum = 0.;
+                if rnuneg.abs() < 750. {
+                    sum += f64::from(s * (ga / (ga_sq + rnuneg.powi(2)) - base));
+                }
+                if rnupos.abs() <= 750. {
+                    sum += f64::from(s * (ga / (ga_sq + rnupos.powi(2)) - base));
+                }
+                sum
+            } else {
+                // modification 2
+                let chi = if freq < 19. {
+                    let u = f32::clamp((freq - 19.).abs() / 16.5, 0., 1.);
+                    0.07 * ga + 0.93 * ga * u.powi(2) * (3. - 2. * u)
+                } else {
+                    0.07 * ga
+                };
 
-    //   gamh2o=0.1820*freq*sftot
+                let chi_sq = chi.powi(2);
+                f64::from(
+                    s * 2. * ((ga - chi) * freq_sq + (ga + chi) * (f0sq + ga_sq - chi_sq))
+                        / ((freq_sq - f0sq - ga_sq + chi_sq).powi(2) + 4. * freq_sq * ga_sq),
+                )
+            }
+        })
+        .sum();
+    let sum = sum.max(0.);
 
-    // TODO
-    0.0
+    let ffac = if freq < 90. {
+        1. + 0.1 * ((90. - freq) / 90.).powf(1.4)
+    } else {
+        1.
+    };
+
+    // modification 3
+    let sftot = pwet
+        * freq
+        * tht.powf(3.5)
+        * (sum
+            + f64::from(ffac * 1.1 * 1.2957246e-6 * pdry / tht.sqrt())
+            + f64::from(0.348 * (freq.powf(0.15)) * 4.2952193e-5 * pwet * tht.powi(4)))
+            as f32;
+
+    0.1820 * freq * sftot
 }
 
 /// Liquid cloud water absorption coefficient.
