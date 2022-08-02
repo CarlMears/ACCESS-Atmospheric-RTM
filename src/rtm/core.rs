318fb9aed86a9baa44c6c6f51af5e4e19b129f08
Richard Lindsley <lindsley@remss.com>
2022-08-01 20:58:13 -0700

    Implement liquid cloud absorption coefficients

--- src/rtm/core.rs
+++ src/rtm/core.rs
@@ -2,6 +2,7 @@
 //!
 //! These are pretty directly re-written from the original Fortran source.
 
+use num_complex::Complex32;
 use once_cell::sync::OnceCell;
 use smallvec::SmallVec;
 
@@ -34,6 +35,66 @@ struct WaterVaporCoefficients {
 
 /// Water vapor absorption coefficients
 static H2O_COEF: OnceCell<WaterVaporCoefficients> = OnceCell::new();
+
+/// Compute total atmospheric parameters from level data.
+///
+/// For an Earth incidence angle `inc` in degrees, and profile data where `t` is
+/// the temperature in K, `z` is the elevation in m, and `tabs` is the
+/// atmospheric absorption coefficient in Np/m, compute the output tuple
+/// (`tran`, `tb_up`, `tb_down`) for the atmospheric transmissivity, atmospheric
+/// upwelling brightness temperature in K, and atmospheric downwelling
+/// brightness temperature in K.
+///
+/// The three profile inputs (`t`, `z`, and `tabs`) all have the same length,
+/// `num_levels + 1`, where the first index `0` is the value at the surface and
+/// indices from `1..=num_levels` are profile data above the surface.
+pub(crate) fn atm_tran(inc: f32, t: &[f32], z: &[f32], tabs: &[f32]) -> (f32, f32, f32) {
+    const DELTA: f32 = 0.00035;
+
+    // Differential slant height
+    let dsdh = (1.0 + DELTA) / f32::sqrt(inc.to_radians().cos().powi(2) + DELTA * (2.0 + DELTA));
+
+    // Number of levels *not* including the surface
+    let num_levels = t.len() - 1;
+
+    let opacity: SmallVec<[f32; 64]> = (1..=num_levels)
+        .into_iter()
+        .map(|i| -dsdh * 0.5 * (tabs[i - 1] + tabs[i]) * (z[i] - z[i - 1]))
+        .collect();
+    let t_avg: SmallVec<[f32; 64]> = (1..=num_levels)
+        .into_iter()
+        .map(|i| 0.5 * (t[i - 1] + t[i]))
+        .collect();
+    let ems: SmallVec<[f32; 64]> = opacity.iter().map(|opacity| 1.0 - opacity.exp()).collect();
+
+    let (sum_down, _sum_op) =
+        (1..=num_levels)
+            .into_iter()
+            .fold((0., 0.), |(sum_down, sum_op), i| {
+                (
+                    sum_down + (t_avg[i - 1] - t[1]) * ems[i - 1] * f32::exp(sum_op),
+                    sum_op + opacity[i - 1],
+                )
+            });
+
+    let (sum_up, sum_op) =
+        (1..=num_levels)
+            .into_iter()
+            .rev()
+            .fold((0., 0.), |(sum_up, sum_op), i| {
+                (
+                    sum_up + (t_avg[i - 1] - t[1]) * ems[i - 1] * f32::exp(sum_op),
+                    sum_op + opacity[i - 1],
+                )
+            });
+
+    let tran = sum_op.exp();
+    let tb_avg = (1. - tran) * t[1];
+    let tb_down = tb_avg + sum_down;
+    let tb_up = tb_avg + sum_up;
+
+    (tran, tb_up, tb_down)
+}
 
 /// Modified version of Liebe 1992 oxygen model.
 ///
@@ -321,77 +382,156 @@ pub(crate) fn abh2o_rk_modified(p: f32, t: f32, pv: f32, freq: f32) -> f32 {
 /// For a frequency `freq` in GHz, a temperature `t` in K, and a liquid cloud
 /// water density `rhol` in g/m^3, compute the cloud water absorption
 /// coefficient in Np/km.
-pub(crate) fn fdcldabs(_freq: f32, _t: f32, _rhol: f32) -> f32 {
-    //   real(real32) :: rhol0, wavlen
-    //   real(real32), parameter :: c=29.979, pi=3.14159
-    //   complex(real32) :: permit
+pub(crate) fn fdcldabs(freq: f32, t: f32, rhol: f32) -> f32 {
+    const C: f32 = 29.979;
+    use std::f32::consts::PI;
 
-    //   rhol0 = 1.0e-6*rhol ![g/cm**3]
-    //   call meissner(freq,t,0.0,   permit)
-    //   wavlen = c/freq
-    //   al = (6.0*pi*rhol0/wavlen)*aimag((1.0-permit)/(2.0+permit))  ! Np/cm
-    //   al = 1.0e5*al ! Np/km
+    // Convert g/m^3 to g/cm^3
+    let rhol0 = 1.0e-6 * rhol;
 
-    // TODO
-    0.0
+    let permit = meissner(freq, t, 0.0);
+    let wavlen = C / freq;
+    // Np/cm
+    let al = (6.0 * PI * rhol0 / wavlen) * ((1.0 - permit) / (2.0 + permit)).im;
+
+    // Convert to Np/km
+    al * 1.0e5
 }
 
-/// Compute total atmospheric parameters from level data.
+/// Compute the complex dielectric constant of water.
 ///
-/// For an Earth incidence angle `inc` in degrees, and profile data where `t` is
-/// the temperature in K, `z` is the elevation in m, and `tabs` is the
-/// atmospheric absorption coefficient in Np/m, compute the output tuple
-/// (`tran`, `tb_up`, `tb_down`) for the atmospheric transmissivity, atmospheric
-/// upwelling brightness temperature in K, and atmospheric downwelling
-/// brightness temperature in K.
+/// For a frequency `freq` in GHz, SST `t` in K, salinity `s` in parts per
+/// thousand, compute the complex dielectric constant of water.
 ///
-/// The three profile inputs (`t`, `z`, and `tabs`) all have the same length,
-/// `num_levels + 1`, where the first index `0` is the value at the surface and
-/// indices from `1..=num_levels` are profile data above the surface.
-pub(crate) fn atm_tran(inc: f32, t: &[f32], z: &[f32], tabs: &[f32]) -> (f32, f32, f32) {
-    const DELTA: f32 = 0.00035;
+/// The ranges for the inputs are:
+///
+/// - `freq`: 1 to 400 GHz
+/// - `t`: -25 °C to 40 °C (or 248.16 K to 313.16 K) for pure water; -2 °C to 34
+///   °C (or 271.16 K to 307.16 K) for saline water
+/// - `s`: 0 to 40 ppt
+///
+/// From Thomas Meissner, February 2002 and October 2004.
+///
+/// The imaginary part is negative to be consistent with "wentz1" convention.
+fn meissner(freq: f32, t: f32, s: f32) -> Complex32 {
+    const F0: f32 = 17.97510;
 
-    // Differential slant height
-    let dsdh = (1.0 + DELTA) / f32::sqrt(inc.to_radians().cos().powi(2) + DELTA * (2.0 + DELTA));
+    // Convert from K to °C
+    let sst = t - 273.15;
+    let (e0s, e1s, e2s, n1s, n2s, sig) = dielectric_meissner_wentz(sst, s);
 
-    // Number of levels *not* including the surface
-    let num_levels = t.len() - 1;
+    // Debye law (2 relaxation wavelengths)
+    let eps = (e0s - e1s) / (1.0 - Complex32::i() * (freq / n1s))
+        + (e1s - e2s) / (1.0 - Complex32::i() * (freq / n2s))
+        + e2s
+        + Complex32::i() * sig * F0 / freq;
 
-    let opacity: SmallVec<[f32; 64]> = (1..=num_levels)
-        .into_iter()
-        .map(|i| -dsdh * 0.5 * (tabs[i - 1] + tabs[i]) * (z[i] - z[i - 1]))
-        .collect();
-    let t_avg: SmallVec<[f32; 64]> = (1..=num_levels)
-        .into_iter()
-        .map(|i| 0.5 * (t[i - 1] + t[i]))
-        .collect();
-    let ems: SmallVec<[f32; 64]> = opacity.iter().map(|opacity| 1.0 - opacity.exp()).collect();
+    eps.conj()
+}
 
-    let (sum_down, _sum_op) =
-        (1..=num_levels)
-            .into_iter()
-            .fold((0., 0.), |(sum_down, sum_op), i| {
-                (
-                    sum_down + (t_avg[i - 1] - t[1]) * ems[i - 1] * f32::exp(sum_op),
-                    sum_op + opacity[i - 1],
-                )
-            });
+/// Complex dielectric constant.
+///
+/// For an input SST, `sst` in °C and salinity `s` in parts-per-thousand,
+/// compute the complex dielectric constant. Returns the tuple `(e0s, e1s, e2s,
+/// n1s, n2s, sig)`.
+///
+/// The allowed values of the inputs are:
+///
+/// - `sst`: from -25°C to 40°C for pure water; -2°C to 34°C for saline water
+/// - `s`: from 0 to 40 ppt
+///
+///
+/// References: T. Meissner  and F. Wentz, IEEE TGARS, 42(9), 2004, 1836-1849.
+fn dielectric_meissner_wentz(sst: f32, s: f32) -> (f32, f32, f32, f32, f32, f32) {
+    const X: [f32; 11] = [
+        5.7230e+00,
+        2.2379e-02,
+        -7.1237e-04,
+        5.0478e+00,
+        -7.0315e-02,
+        6.0059e-04,
+        3.6143e+00,
+        2.8841e-02,
+        1.3652e-01,
+        1.4825e-03,
+        2.4166e-04,
+    ];
 
-    let (sum_up, sum_op) =
-        (1..=num_levels)
-            .into_iter()
-            .rev()
-            .fold((0., 0.), |(sum_up, sum_op), i| {
-                (
-                    sum_up + (t_avg[i - 1] - t[1]) * ems[i - 1] * f32::exp(sum_op),
-                    sum_op + opacity[i - 1],
-                )
-            });
+    const Z: [f32; 13] = [
+        -3.56417e-03,
+        4.74868e-06,
+        1.15574e-05,
+        2.39357e-03,
+        -3.13530e-05,
+        2.52477e-07,
+        -6.28908e-03,
+        1.76032e-04,
+        -9.22144e-05,
+        -1.99723e-02,
+        1.81176e-04,
+        -2.04265e-03,
+        1.57883e-04,
+    ];
 
-    let tran = sum_op.exp();
-    let tb_avg = (1. - tran) * t[1];
-    let tb_down = tb_avg + sum_down;
-    let tb_up = tb_avg + sum_up;
+    const A0_COEF: [f32; 3] = [-0.33330E-02, 4.74868e-06, 0.0e0];
+    const B1_COEF: [f32; 5] = [
+        0.23232E-02,
+        -0.79208E-04,
+        0.36764E-05,
+        -0.35594E-06,
+        0.89795E-08,
+    ];
 
-    (tran, tb_up, tb_down)
+    // protects against n1 and n2 going zero for very cold water
+    let sst = sst.max(-30.16);
+    let sst2 = sst.powi(2);
+    let sst3 = sst.powi(3);
+    let sst4 = sst.powi(4);
+
+    let s2 = s.powi(2);
+
+    // Pure water. e0 is from Stogryn et al.
+    let e0 = (3.70886e4 - 8.2168e1 * sst) / (4.21854e2 + sst);
+    let e1 = X[0] + X[1] * sst + X[2] * sst2;
+    let n1 = (45.0 + sst) / (X[3] + X[4] * sst + X[5] * sst2);
+    let e2 = X[6] + X[7] * sst;
+    let n2 = (45.0 + sst) / (X[8] + X[9] * sst + X[10] * sst2);
+
+    // Saline water. Conductivity [s/m] taken from Stogryn et al.
+    let sig35 =
+        2.903602 + 8.60700e-2 * sst + 4.738817e-4 * sst2 - 2.9910e-6 * sst3 + 4.3047e-9 * sst4;
+    let r15 = s * (37.5109 + 5.45216 * s + 1.4409e-2 * s2) / (1004.75 + 182.283 * s + s2);
+
+    let alpha0 = (6.9431 + 3.2841 * s - 9.9486e-2 * s2) / (84.850 + 69.024 * s + s2);
+    let alpha1 = 49.843 - 0.2276 * s + 0.198e-2 * s2;
+    let rtr15 = 1.0 + (sst - 15.0) * alpha0 / (alpha1 + sst);
+
+    let sig = sig35 * r15 * rtr15;
+
+    // permittivity
+    let a0 = f32::exp(A0_COEF[0] * s + A0_COEF[1] * s2 + A0_COEF[2] * s * sst);
+    let e0s = a0 * e0;
+
+    let b1 = if sst <= 30. {
+        1.0 + s
+            * (B1_COEF[0]
+                + B1_COEF[1] * sst
+                + B1_COEF[2] * sst2
+                + B1_COEF[3] * sst3
+                + B1_COEF[4] * sst4)
+    } else {
+        1.0 + s * (9.1873715e-04 + 1.5012396e-04 * (sst - 30.))
+    };
+    let n1s = n1 * b1;
+
+    let a1 = f32::exp(Z[6] * s + Z[7] * s2 + Z[8] * s * sst);
+    let e1s = e1 * a1;
+
+    let b2 = 1.0 + s * (Z[9] + 0.5 * Z[10] * (sst + 30.));
+    let n2s = n2 * b2;
+
+    let a2 = 1.0 + s * (Z[11] + Z[12] * sst);
+    let e2s = e2 * a2;
+
+    (e0s, e1s, e2s, n1s, n2s, sig)
 }
